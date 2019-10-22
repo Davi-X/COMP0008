1f23394dd10f3be3e3b91cc75ca27315b3c2a8a9
Davies Xue <zcabxue@ucl.ac.uk>
2019-10-22 22:06:57 +0100

    Array

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a = pow(2,30);
+    printf("%d",a<<1);
+}
---
+++ test.c++
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 2^30;
+    printf("%d",a);
+}
